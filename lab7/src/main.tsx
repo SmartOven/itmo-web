1c1d11928a8339c4e7dabe0a2b8f5d13a6ace126
smartoven <yarik.panteleev@gmail.com>
2023-11-29 20:21:51 +0300

    lab6-7 убрал неработающий спиннер

--- lab7/src/main.tsx
+++ lab7/src/main.tsx
@@ -3,12 +3,9 @@ import './styles/main.css'
 import App from "./App.tsx";
 import {store} from "./features/redux/store.ts";
 import {Provider} from "react-redux";
-// @ts-ignore Нет типизации - TS ругается
-import { Lines } from "react-preloaders";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
         <App/>
-        <Lines />
     </Provider>,
 )
