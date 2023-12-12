9bb50c553ca2d52a37372af86db747890c7e4e5e
smartoven <yarik.panteleev@gmail.com>
2023-12-12 21:56:11 +0300

    lab4 fix

--- lab4/scripts/main.js
+++ lab4/scripts/main.js
@@ -1,11 +1,11 @@
-(function() {
+(function () {
     // Function to add 'active' class to the current menu item
     function setActiveMenuItem() {
         const pathParts = window.location.pathname.split('/');
         const page = pathParts[pathParts.length - 1];
         const menuItems = document.querySelectorAll('nav ul li a');
 
-        menuItems.forEach(function(item) {
+        menuItems.forEach(function (item) {
             if (item.getAttribute('href') === page) {
                 item.classList.add('active');
                 item.classList.remove('not-active');
@@ -16,22 +16,19 @@
         });
     }
 
-    // FIXME Не работает, работает только внутри html файла
     // Function to display loading time in the footer
     function displayLoadingTime() {
-        const start = new Date().getTime();
-        window.onload = () => {
-            const end = new Date().getTime();
-            let loadTime = end - start
-            const footer = document.querySelector('footer');
-            const loadingInfo = document.createElement('p');
-            loadingInfo.textContent = 'Page loaded in ' + loadTime + ' milliseconds.';
-            footer.appendChild(loadingInfo);
-        };
+        const end = new Date().getTime();
+        let loadTime = end - start
+        const footer = document.querySelector('footer');
+        const loadingInfo = document.createElement('p');
+        loadingInfo.textContent = 'Page loaded in ' + loadTime + ' milliseconds.';
+        footer.appendChild(loadingInfo);
     }
 
+    const start = new Date().getTime();
     // Event listener for page load
-    window.addEventListener('load', function() {
+    window.addEventListener('load', function () {
         setActiveMenuItem();
         displayLoadingTime();
     });
